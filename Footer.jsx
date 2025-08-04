d3f87e545bf39ed8ac9b77f70de82207b9820975
Sharmenserem <shammelody43@gmail.com>
2025-08-04 23:39:54 +0300

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    <p>© 2025 Sharmen Project</p>
+    <p>Made with love in Kenya 🇰🇪</p>
+  </footer>
+);
+
+export default Footer;
