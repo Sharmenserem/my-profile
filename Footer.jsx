bb5bbc978720f2658459df30e6fe3ac5d374ba97
Sharmenserem <shammelody43@gmail.com>
2025-08-05 00:18:15 +0300

    Update Footer.jsx

--- Footer.jsx
+++ Footer.jsx
@@ -1,10 +1,1 @@
-import React from 'react';
 
-const Footer = () => (
-  <footer className="footer">
-    <p>© 2025 Sharmen Project</p>
-    <p>Made with love in Kenya 🇰🇪</p>
-  </footer>
-);
-
-export default Footer;
