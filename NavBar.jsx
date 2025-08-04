33e977dab4866f764e947766279665e6a403497e
Sharmenserem <shammelody43@gmail.com>
2025-08-05 00:13:52 +0300

    Update NavBar.jsx

--- NavBar.jsx
+++ NavBar.jsx
@@ -1,12 +1,1 @@
-import React from 'react';
 
-const NavBar = () => (
-  <nav className="navbar">
-    <button>🏠 Home</button>
-    <button>🎧 Stream</button>
-    <button>⬇️ Download</button>
-    <button>📂 Menu</button>
-  </nav>
-);
-
-export default NavBar;
