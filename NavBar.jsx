98128a047127ff00e5065ec93f079e6419c96c7c
Sharmenserem <shammelody43@gmail.com>
2025-08-04 23:36:37 +0300

    Create NavBar.jsx

---
+++ NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavBar = () => (
+  <nav className="navbar">
+    <button>🏠 Home</button>
+    <button>🎧 Stream</button>
+    <button>⬇️ Download</button>
+    <button>📂 Menu</button>
+  </nav>
+);
+
+export default NavBar;
